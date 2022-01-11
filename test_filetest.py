ec9948ff989b0f965d07ed33a01c0b550f5ad2e8
synovialbasher <synovialbasher@gmail.com>
2022-01-11 10:05:18 +0100

    Modified testfile and pytest

--- test_filetest.py
+++ test_filetest.py
@@ -1,11 +1,11 @@
 import pytest
-import os
+import pytest_console_scripts import ScriptRunner
 
 def test01_pytest_selftest():
     print("Pytest is now running")
     assert True
     
 def test02_testfile_output():
-    assert "This is a test file" in os.system('python testfile.py').stdout()
-     
-    
+    ret = script_runner.run('testfile.py', print_result=True, shell=True)
+    assert ret.success
+    assert ret.stdout == "This is my test file!"
